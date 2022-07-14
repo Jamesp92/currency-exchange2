7772891c9573d9b30a7fd461b9809c38b1a88eb3
pj perry <pjperry28@gmail.com>
2022-07-13 20:54:30 -0700

    install env plug in

--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -1,1 +1,31 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyElement from './currency-Service.js';
 
+ 
+function getElements(response){
+   if (response.ok){
+    let userCurrency = $("#userDollar")
+    let convertTo = $("#currencySelect")
+    $('#answer').text(parseInt(response.conversion_rates[convertTo] * userCurrency));
+   }
+   else
+   {
+    $(".showErrors").text(' please try again ')
+   }
+}
+
+async function makeApiCall(){
+  const response = await CurrencyElement.currencyApi(convertTo);
+  getElements(response)
+}
+
+$(document).ready(function() {
+  $('#button').click(function() {
+ event.preventDefault();
+    makeApiCall(convertTo);
+    $(".showErrors")
+  });
+});
