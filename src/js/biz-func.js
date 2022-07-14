8ef367e043732c134b64fed1a41d195798a8d342
pj perry <pjperry28@gmail.com>
2022-07-13 20:32:04 -0700

    build out html and bring over service code from other repo

--- src/js/biz-func.js
+++ src/js/biz-func.js
@@ -1,8 +1,1 @@
-// import $ from 'jquery';
 
-const exampleTestFunction = () => {
-  console.log('hi there, from the deck');
-  return 'hi there, from the deck';
-};
-
-export default exampleTestFunction;
