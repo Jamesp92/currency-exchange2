3ab0d975a8061d016a50d13609775c0e138986c7
pj perry <pjperry28@gmail.com>
2022-07-17 17:18:45 -0700

    add text to out converted amount also add some css styling

---
+++ src/currency-Service.js
@@ -0,0 +1,17 @@
+export default class CurrencyElement{
+  static async currencyApi() {
+    try {
+      let url =(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      let request = await fetch(url);
+      if (request.ok && request.status === 200) {
+        let response = await request.json();
+        return response;
+      } else {
+        request = false;
+      }    
+    } catch (error) {
+      return false;
+    }
+  }
+}
+ 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-// import bizFunc from './js/biz-func.js';
-// import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import CurrencyElement from './currency-Service.js';
 
-$(document).ready(function () {
-  $('selector').submit(function (event) {
-    event.preventDefault(); // if form
-    // more code
+ 
+function getElements(response){
+  
+  if (response){
+    let userCurrency = $("#userDollar").val();
+    let convertTo = $("#currencySelect").val();
+    $('#answer').text(" your converted amount is :" + " "  + parseInt(response.conversion_rates[convertTo] * userCurrency));
+  }
+  else
+  {
+    $(".showErrors").text(' please try again ');
+  }
+}
+
+async function makeApiCall(convertTo){
+  const response = await CurrencyElement.currencyApi(convertTo);
+  getElements(response);
+}
+
+$(document).ready(function() {
+  $('#button').click(function() {
+    event.preventDefault();
+    makeApiCall();
+    $(".showErrors");
   });
 });
